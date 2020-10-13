1d4a993544180896f1806701d22086b1b1e74ab1
aayamneupane <aayamkumarneupane@gmail.com>
2020-10-13 10:10:00 +0545

    Created details of the Employee

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+public class EmployeeController : Controller
+{
+    public ActionResult Index()
+    {
+        //List<Person> employees = new List<Person>();
+
+        //Object initializer syntax
+        
+
+        List<Person> employees = Person.GetPerson();
+        return View(employees);
+    }
+
+    public ActionResult Detail(string FirstName)
+    {
+        var employee = Person.GetPerson();
+        var reqEmployee = from Person in employee where Person.FirstName == FirstName select Person;
+        return View(reqEmployee.First());
+    }
+}
+
---
+++ EmployeeManagement/Models/Person.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string Surname { get; set; } 
+    public string Address { get; set; }
+    public double Salary { get; set; }
+    public char Gender { get; set; } = 'M';
+    
+    public static List<Person> GetPerson()
+    {
+        Person emp1 = new Person()
+        {
+            FirstName = "Aayam",
+            Surname = "Neupane",
+            Address = "Jagati",
+            Salary = 100000.0
+        }; 
+        Person emp2 = new Person()
+        {
+            FirstName = "Atanka",
+            Surname = "Neupane",
+            Address = "Jagati",
+            Salary = 100000.0
+        }; 
+        Person emp3 = new Person()
+        {
+            FirstName = "Sandhya",
+            Surname = "Neupane",
+            Address = "Jagati",
+            Salary = 100000.0
+        }; 
+        Person emp4 = new Person()
+        {
+            FirstName = "Anjali",
+            Surname = "Neupane",
+            Address = "Jagati",
+            Salary = 100000.0
+        }; 
+
+        List<Person> employees = new List<Person> {emp1,emp2,emp3,emp4};
+        return employees;
+    }
+}
